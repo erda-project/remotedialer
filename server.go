54da327b22e19b1fef38e7c0bafec669361e9b42
CeerDecy <1748788674@qq.com>
2024-07-17 09:51:41 +0800

    feat: middleWare func

--- server.go
+++ server.go
@@ -27,15 +27,26 @@ func DefaultErrorWriter(rw http.ResponseWriter, req *http.Request, code int, err
 	rw.Write([]byte(err.Error()))
 }
 
+type HandlerFunc func(ctx *Context)
+
+type MiddleFunc func(next HandlerFunc) HandlerFunc
+
 type Server struct {
 	PeerID                  string
 	PeerToken               string
 	ClientConnectAuthorizer ConnectAuthorizer
 	authorizer              Authorizer
+	middleFunc              []MiddleFunc
 	errorWriter             ErrorWriter
 	sessions                *sessionManager
 	peers                   map[string]peer
 	peerLock                sync.Mutex
+}
+
+type Context struct {
+	RW      http.ResponseWriter
+	Req     *http.Request
+	Session *Session
 }
 
 func New(auth Authorizer, errorWriter ErrorWriter) *Server {
@@ -47,52 +58,78 @@ func New(auth Authorizer, errorWriter ErrorWriter) *Server {
 	}
 }
 
-func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	clientKey, authed, peer, proxy, err := s.auth(req)
-	if err != nil {
-		s.errorWriter(rw, req, 400, err)
-		return
-	}
-	if !authed {
-		s.errorWriter(rw, req, 401, errFailedAuth)
-		return
-	}
-
-	logrus.Infof("Handling backend connection request [%s]", clientKey)
-
-	upgrader := websocket.Upgrader{
-		HandshakeTimeout: 5 * time.Second,
-		CheckOrigin:      func(r *http.Request) bool { return true },
-		Error:            s.errorWriter,
-	}
-
-	wsConn, err := upgrader.Upgrade(rw, req, nil)
-	if err != nil {
-		s.errorWriter(rw, req, 400, errors.Wrapf(err, "Error during upgrade for host [%v]", clientKey))
-		return
-	}
-	var session *Session
-	if !proxy {
-		session = s.sessions.add(clientKey, wsConn, peer)
-		defer s.sessions.remove(session)
-	} else {
-		session = NewProxySession(func(string, string) bool { return true }, wsConn)
-		session.dialer = func(ctx context.Context, network, address string) (net.Conn, error) {
-			parts := strings.SplitN(network, "::", 2)
-			if len(parts) != 2 {
-				return nil, fmt.Errorf("invalid clientKey/proto: %s", network)
-			}
-			d := s.Dialer(parts[0])
-			return d(ctx, parts[1], address)
+func (s *Server) WithAuthorizer(next HandlerFunc) HandlerFunc {
+	return func(ctx *Context) {
+		clientKey, authed, peer, proxy, err := s.auth(ctx.Req)
+		if err != nil {
+			s.errorWriter(ctx.RW, ctx.Req, 400, err)
+			return
 		}
-		defer session.Close()
+		if !authed {
+			s.errorWriter(ctx.RW, ctx.Req, 401, errFailedAuth)
+			return
+		}
+
+		logrus.Infof("Handling backend connection request [%s]", clientKey)
+
+		upgrader := websocket.Upgrader{
+			HandshakeTimeout: 5 * time.Second,
+			CheckOrigin:      func(r *http.Request) bool { return true },
+			Error:            s.errorWriter,
+		}
+
+		wsConn, err := upgrader.Upgrade(ctx.RW, ctx.Req, nil)
+		if err != nil {
+			s.errorWriter(ctx.RW, ctx.Req, 400, errors.Wrapf(err, "Error during upgrade for host [%v]", clientKey))
+			return
+		}
+		var session *Session
+		if !proxy {
+			session = s.sessions.add(clientKey, wsConn, peer)
+			defer s.sessions.remove(session)
+		} else {
+			session = NewProxySession(func(string, string) bool { return true }, wsConn)
+			session.dialer = func(ctx context.Context, network, address string) (net.Conn, error) {
+				parts := strings.SplitN(network, "::", 2)
+				if len(parts) != 2 {
+					return nil, fmt.Errorf("invalid clientKey/proto: %s", network)
+				}
+				d := s.Dialer(parts[0])
+				return d(ctx, parts[1], address)
+			}
+			defer session.Close()
+		}
+		session.auth = s.ClientConnectAuthorizer
+		ctx.Session = session
+
+		next(ctx)
 	}
-	session.auth = s.ClientConnectAuthorizer
-	code, err := session.Serve(req.Context())
-	if err != nil {
-		// Hijacked so we can't write to the client
-		logrus.Infof("error in remotedialer server [%d]: %v", code, err)
+}
+
+func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	handle := func(ctx *Context) {
+		if ctx.Session == nil {
+			logrus.Infof("No Session found for request [%s]", ctx.Req.URL.Path)
+			return
+		}
+		code, err := ctx.Session.Serve(req.Context())
+		if err != nil {
+			// Hijacked so we can't write to the client
+			logrus.Infof("error in remotedialer server [%d]: %v", code, err)
+		}
 	}
+
+	for _, f := range s.middleFunc {
+		handle = f(handle)
+	}
+
+	ctx := &Context{
+		RW:  rw,
+		Req: req,
+	}
+
+	handle(ctx)
 }
 
 func (s *Server) auth(req *http.Request) (clientKey string, authed, peer, proxy bool, err error) {
